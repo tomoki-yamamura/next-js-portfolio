2c28104f74ba4b681a93f882677fd11d108b3dcc
tomoki-yamamura <progtab17@gmail.com>
2024-04-17 17:50:21 +0800

    finish styling

--- src/app/components/HeroSection.jsx
+++ src/app/components/HeroSection.jsx
@@ -9,21 +9,25 @@ const HeroSection = () => {
           <h1 className="text-white mb-4 text-4xl lg:text-6xl font-extrabold">
             I'm Tomoki
           </h1>
-          <p className="text-[#ADB7BE] text-lg lg:text-xl">
+          <p className="text-[#ADB7BE] mb-6 text-lg lg:text-xl">
             Lorem Ipsum is simply dummy text of the printing and typesetting
             industry. Lorem Ipsum has been the industry's standard dummy text
             ever since the 1500s, when an unknown printer took a galley of type
             and scrambled it to make a type specimen book.
           </p>
+          <div>
+            <button className="px-6 py-3 rounded-full mr-4 bg-white hover:bg-slate-200 text-black">Hire me</button>
+            <button className="px-6 py-3 rounded-full bg-transparent hover:bg-slate-800 text-white border border-white mt-3">Download</button>
+          </div>
         </div>
-        <div className="col-span-5">
-          <div className="rounded-full bg-[#181818] w-500 h-400 relative">
+        <div className="col-span-5 place-self-center mt-4 lg:mt-0">
+          <div className="rounded-full bg-[#181818] w-[250px] h-[250px] lg:w-[400px] lg:h-[400px] relative">
             <Image
               src="/images/hero-image.jpeg"
               alt="hero image"
               width={300}
               height={300}
-              className="absolute transform -translate-x-1/2"
+              className="absolute transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
             />
           </div>
         </div>
