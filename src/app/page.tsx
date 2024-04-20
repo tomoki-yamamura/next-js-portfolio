93d4c67073fe441bd1c39e79bf872464fa0f7fba
tomoki-yamamura <progtab17@gmail.com>
2024-04-21 00:48:31 +0800

    fadein

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,40 @@
-// import Image from "next/image";
-// import HeroSection from "./components/HeroSection"
-// import AboutSection from "./components/AboutSection"
-// import ProjectsSection from "./components/ProjectsSection";
-// import Navbar from "./components/Navbar"
+"use client"
+import Image from "next/image";
+import { useEffect, useState } from "react";
+import HeroSection from "./components/HeroSection"
+import AboutSection from "./components/AboutSection"
+import ProjectsSection from "./components/ProjectsSection";
+import Navbar from "./components/Navbar"
+import Intro from "./components/Intro"
+import "../styles/home.css"
+export default function Home() {
+  const [isLoading, setIsLoading] = useState(true);
+  useEffect(() => {
+    setIsLoading(true)
+    setTimeout(() => {
+      setIsLoading(false)
+    }, 2 * 1000);
+  }, [])
+
+  return (
+    isLoading ? (
+      <div>
+        <Intro />
+      </div>
+    ) : (
+      <main className="fadeIn flex min-h-screen flex-col bg-[#121212]">
+        <Navbar />
+        <div className="container mx-auto px-12">
+          <HeroSection />
+          <AboutSection />
+          <ProjectsSection />
+        </div>
+      </main>
+    )
+  );
+};
+
+
 
 // export default function Home() {
 //   return (
@@ -18,37 +50,3 @@
 //     </main>
 //   );
 // }
-"use client"
-import { useEffect, useState } from "react";
-import Intro from "./components/Intro"
-export default function Home() {
-  const [isLoading, setIsLoading] = useState(true);
-  useEffect(() => {
-    setIsLoading(true)
-    setTimeout(() => {
-      setIsLoading(false)
-    }, 2 * 1000);
-  }, [])
-
-  return(
-    <Layout home>
-      <Head>
-        <title>{siteTitle}</title>
-      </Head>
-      {isLoading ?
-      <div>
-         //Load中の処理
-      </div>
-      : <div>
-         //Load後の処理
-      </div>
-      }
-    </Layout>
-  )
-
-  return (
-    {isLoading ?
-      <Intro />
-    }
-  )
-};
