fe9b5f9b9e084348223b32b03ac65b76c833c20f
tomoki-yamamura <progtab17@gmail.com>
2024-04-20 23:59:00 +0800

    intro

---
+++ src/app/components/Intro.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "../../styles/Intro.module.css";
+
+const Intro = () => {
+  return (
+    <div className="w-full h-full flex justify-center items-center" id="stars">
+      <p className=" text-white">A few days ago, during...</p>;
+    </div>
+  );
+};
+
+export default Intro;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,28 @@
-import Image from "next/image";
-import HeroSection from "./components/HeroSection"
-import AboutSection from "./components/AboutSection"
-import ProjectsSection from "./components/ProjectsSection";
-import Navbar from "./components/Navbar"
+// import Image from "next/image";
+// import HeroSection from "./components/HeroSection"
+// import AboutSection from "./components/AboutSection"
+// import ProjectsSection from "./components/ProjectsSection";
+// import Navbar from "./components/Navbar"
 
+// export default function Home() {
+//   return (
+//     <main >
+//       <main className="flex min-h-screen flex-col bg-[#121212]">
+//         <Navbar />
+//         <div className="container mx-auto px-12">
+//           <HeroSection />
+//           <AboutSection />
+//           <ProjectsSection />
+//         </div>
+//       </main>
+//     </main>
+//   );
+// }
+import Intro from "./components/Intro"
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col bg-[#121212]">
-      <Navbar />
-      <div className="container mx-auto px-12">
-        <HeroSection />
-        <AboutSection />
-        <ProjectsSection />
-      </div>
+    <main className="h-screen overflow-hidden bg-[#090a0f]">
+      <Intro />
     </main>
   );
-}
+};
